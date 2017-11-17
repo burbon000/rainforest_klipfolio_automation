dfd7778f121721b4936988c529502e2d2d9b6087
Brian Urbon <burbon000@users.noreply.github.com>
2017-11-17 04:26:13 -0800

    updated change_public_to_private with new step 3 response text

--- change_public_link_to_private.rb
+++ change_public_link_to_private.rb
@@ -5,7 +5,19 @@
 test(id: 90753, title: "Changing public link to a private link") do
   # You can use any of the following variables in your code:
   # - []
-  
+  Capybara.register_driver :sauce do |app|
+    @desired_cap = {
+      'platform': "Windows 7",
+      'browserName': "firefox",
+      'version': "45",
+      'name': "klipfolio_change_public_link",
+    }
+    Capybara::Selenium::Driver.new(app,
+      :browser => :remote,
+      :url => 'http://RFAutomation:5328f84f-5623-41ba-a81e-b5daff615024@ondemand.saucelabs.com:80/wd/hub',
+      :desired_capabilities => @desired_cap
+    )
+  end  
   rand_num=Random.rand(899999999) + 100000000
   link_text = ''
   visit "https://app.Klipfolio.com/"
@@ -34,7 +46,7 @@ test(id: 90753, title: "Changing public link to a private link") do
     expect(page).to have_content('My Dashboards')
     expect(page).to have_content(firstname)
 
-    page.save_screenshot('screenshot_step_1.png')
+    #page.save_screenshot('screenshot_step_1.png')
     # *** STOP EDITING HERE ***
   end
 
@@ -55,15 +67,16 @@ test(id: 90753, title: "Changing public link to a private link") do
     expect(page).to have_selector(:css, '.overlay-container', wait: 30)
     expect(page).to have_content('Share a view-only copy of this dashboard with users outside your account.')
 
-    page.save_screenshot('screenshot_step_2.png')
+    #page.save_screenshot('screenshot_step_2.png')
     # *** STOP EDITING HERE ***
   end
 
   step id: 3,
       action: "Set the Visibility to 'Public and available via search', overwrite the Dashboard Name by typing 'my link',"\
               " set the Theme to DARK, then click on the PUBLISH NOW button.",
-      response: "Do you see a new dialog or a pop up? (You’re about to make this dashboard accessible by the public.)" do
-   
+      #response: "Do you see a new dialog or a pop up? (You’re about to make this dashboard accessible by the public.)" do
+      response: "Do you see a new dialog or a pop up? (Congrats! You're one step away from making your dashboard public.)" do
+
     # *** START EDITING HERE ***
 
     # action
@@ -75,9 +88,11 @@ test(id: 90753, title: "Changing public link to a private link") do
     end
 
     # response
-    expect(page).to have_content('You’re about to make this dashboard accessible by the public.')
-	
-    page.save_screenshot('screenshot_step_3.png')
+
+    expect(page).to have_content("Congrats! You're one step away from making your dashboard public.")
+    #expect(page).to have_content('You’re about to make this dashboard accessible by the public.')
+    
+    #page.save_screenshot('screenshot_step_3.png')
     # *** STOP EDITING HERE ***
   end
 
@@ -94,7 +109,7 @@ test(id: 90753, title: "Changing public link to a private link") do
     # response
     expect(page).to have_content('Link Published')
 
-    page.save_screenshot('screenshot_step_4.png')
+    #page.save_screenshot('screenshot_step_4.png')
     # *** STOP EDITING HERE ***
   end
 
@@ -112,7 +127,7 @@ test(id: 90753, title: "Changing public link to a private link") do
     # response
     expect(page).to have_no_content('Link Published')
 
-    page.save_screenshot('screenshot_step_5.png')
+    #page.save_screenshot('screenshot_step_5.png')
     # *** STOP EDITING HERE ***
   end
 
@@ -131,7 +146,7 @@ test(id: 90753, title: "Changing public link to a private link") do
     # response 
     expect(page).to have_content('my link')
 
-    page.save_screenshot('screenshot_step_6.png')
+    #page.save_screenshot('screenshot_step_6.png')
     # *** STOP EDITING HERE ***
   end
   
@@ -143,12 +158,12 @@ test(id: 90753, title: "Changing public link to a private link") do
     # *** START EDITING HERE ***
 
     # action
-    page.find(:css, 'span', :text => 'my link').click
+    page.find(:css, 'span', :text => 'my link', :match => :first).click
 
     # response
     expect(page).to have_content('Edit Published Link to Dashboard')
     
-    page.save_screenshot('screenshot_step_7.png')
+    #page.save_screenshot('screenshot_step_7.png')
     # *** STOP EDITING HERE ***
 
   end
@@ -168,7 +183,7 @@ test(id: 90753, title: "Changing public link to a private link") do
       expect(page).to have_content('Anyone with link and password')
     end
 
-    page.save_screenshot('screenshot_step_8.png')
+    #page.save_screenshot('screenshot_step_8.png')
     # *** STOP EDITING HERE ***
 
   end
@@ -190,7 +205,7 @@ test(id: 90753, title: "Changing public link to a private link") do
     expect(page).to have_content('The password you entered is invalid.')
 
 
-    page.save_screenshot('screenshot_step_9.png')
+    #page.save_screenshot('screenshot_step_9.png')
     # *** STOP EDITING HERE ***
 
   end
@@ -211,7 +226,7 @@ test(id: 90753, title: "Changing public link to a private link") do
     # response
     expect(page).to have_content('my link')
 
-    page.save_screenshot('screenshot_step_10.png')
+    #page.save_screenshot('screenshot_step_10.png')
     # *** STOP EDITING HERE ***
 
   end
